0cdfbb6daabbc9ad4d325520b6fa567bdc65c23d
Yumin Kim <yumingames@gmail.com>
2023-09-10 17:20:12 +0900

    feat: NodeJS 기본 설정 추가(Pug,  Express)

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+
+console.log("hello");
+
+app.listen(3000);
