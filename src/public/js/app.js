992b68615932ab0dae608272c52ca0a151fa5d9d
Yumin Kim <yumingames@gmail.com>
2023-09-10 21:34:21 +0900

    feat: WebSocket 추가 feat: 간단한 채팅 코드 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,45 @@
-alert("hi");
+const messageList = document.querySelector("ul");
+const nicknameForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Browser");
+});
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected to Browser");
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+
+  const li = document.createElement("li");
+  li.innerText = `You: ${input.value}`;
+  messageList.append(li);
+
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nicknameForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nicknameForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,12 +1,41 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public")); // give permission to access /public folder to user
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-console.log("hello");
+const server = http.createServer(app); // web Server
+const wss = new WebSocket.Server({ server }); // webSocket Server
 
-app.listen(3000);
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  socket.send("hello");
+  socket.on("close", () => console.log("Disconnected from the brow ser"));
+  socket.on("message", (message) => {
+    const packet = JSON.parse(message);
+
+    switch (packet.type) {
+      case "new_message":
+        console.log(packet.payload);
+        sockets.forEach((aSocket) => {
+          aSocket.send(`${socket["nickname"]}: ${packet.payload}`);
+        });
+        break;
+      case "nickname":
+        socket["nickname"] = packet.payload;
+        break;
+    }
+  });
+  console.log(socket);
+});
+
+server.listen(3000);
